46f85bf42d4f2adc31baa33b84d8900e3d3c78b7
ethanbossen <44818827+ethanbossen@users.noreply.github.com>
2024-03-26 16:12:19 -0400

    Moved pinia setup to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,18 @@ import './styles/mug.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import createPinia from "pinia";
+import {defineStore} from "pinia";
+
+export const useStore = defineStore("choiceStore", {
+  state: () => {
+    return {
+      drinkname: "",
+      isCold: "",
+      creamer: "",
+      syrup: "",
+      base: "",
+    };
+  }
+})
